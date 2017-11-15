501896ac4783721434a7c05ba795dd85b8ca90d9
Quentin-Aime <quentin.aime.dev@gmail.com>
2017-11-15 13:40:43 +0100

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,23 @@
 /* fonctions de la world map */
+function  check_all_opacity() {
+  var europe = document.querySelector('.Europe_div')
+  var asie = document.querySelector('.Asie_div')
+  var oceanie = document.querySelector('.Oceanie_div')
+  var amerique_nord = document.querySelector('.Amerique_nord_div')
+  var amerique_sud = document.querySelector('.Amerique_sud_div')
+  var afrique = document.querySelector('.Afrique_div')
+
+  if (europe.style.opacity == 0 && asie.style.opacity == 0
+      && oceanie.style.opacity == 0 && amerique_nord.style.opacity == 0
+      && amerique_sud.style.opacity == 0 && afrique.style.opacity == 0)
+    return true;
+  else
+  {
+    console.log('NON');
+    return false;
+  }
+}
+
 function fadeIn(el) {
       el.style.display = 'block';
 
@@ -33,7 +52,8 @@ function fadeOut(el) {
 
         var el = document.querySelector(string);
 
-        fadeIn(el);
+        if (check_all_opacity() == true)
+          fadeIn(el);
         window.onclick = function(event) {
           if (!(event.target == el) && el.style.opacity == 1)
             {
